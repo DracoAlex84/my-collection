c69cee94029a969418623cff83c67d5b0f0af1e0
DracoAlex84 <draco_alexis@hotmail.com>
2026-01-27 10:19:21 -0300

    link not required

--- src/models/Collections.js
+++ src/models/Collections.js
@@ -69,7 +69,7 @@ const userSchema = new mongoose.Schema({
     user: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User",
-        required: true,
+        required: false,
     }
 
 }, { timestamps: true });
